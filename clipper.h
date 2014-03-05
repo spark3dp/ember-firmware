61a6612f88d44fe71ffe7eef7af186f44eaca527
Richard Greene <richard.greene@autodesk.com>
2014-03-05 15:48:21 -0800

    testing adding files
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 93697]

---
+++ clipper.h
@@ -0,0 +1,7 @@
+/*
+ * clipper.h:
+ *	Implement the Cohen-Suterland line endpoint clipping algorithm
+ ***********************************************************************
+ */
+
+extern int lineClip (int *ix0, int *iy0, int *ix1, int *iy1) ;
