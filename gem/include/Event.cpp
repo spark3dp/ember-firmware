4d5cb91fae966d1e77e5fd2c8196ccc4e583927e
Richard Greene <richard.greene@autodesk.com>
2014-03-27 09:48:48 -0800

    adding event class files
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 94075]

---
+++ gem/include/Event.cpp
@@ -0,0 +1,29 @@
+/*
+ * File:   Event.cpp
+ * Author: Richard Greene
+ * Implements an event that may be subscribed to.
+ * Created on March 26, 2014, 5:43 PM
+ */
+
+#include <Event.h>
+#include <unistd.h>
+
+
+/// Public constructor, sets member variables
+/// [should it also open the file, to create the file descriptor, instead of having it passed in?]
+Event::Event(EventType type, int fileDescriptor, 
+             uint32_t inFlags, uint32_t outFlags)
+{
+    _type = type;
+    _fileDescriptor = fileDescriptor;  // or should it open the file?
+    _inFlags = inFlags;
+    _outFlags = outFlags;
+}
+
+/// Closes the file that signals the event
+Event::~Event()
+{
+    close(_fileDescriptor);
+}
+
+
