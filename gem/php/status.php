ac4bb5f125b93b810624d64c9630baddeb0220ba
Richard Greene <richard.greene@autodesk.com>
2014-05-16 16:57:28 -0800

    adding php fiile that reports printer state or time remaining
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 94927]

---
+++ gem/php/status.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<?php
+
+    $timeLeft = "NA";
+    $handle = @fopen("/tmp/StatusToNetPipe", "r");
+    if ($handle) {
+        $timeLeft = fgets($handle, 4096);
+    }
+    fclose($handle);
+
+    echo $timeLeft;
+?>
+</head>
