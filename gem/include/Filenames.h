fa3d6cb21a42afb160a09f163d7f383968b9a2ed
Richard Greene <richard.greene@autodesk.com>
2014-05-19 10:19:24 -0800

    adding Filenames.h
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 95004]

---
+++ gem/include/Filenames.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   Filenames.h
+ * Author: Richard Greene
+ * 
+ * Names of named pipes and other shared files.
+ *
+ * Created on May 19, 2014, 11:17 AM
+ */
+
+#ifndef FILENAMES_H
+#define	FILENAMES_H
+
+
+
+#endif	/* FILENAMES_H */
+
