5400042c8908bacf7dc67f1d721ac10adbc90141
Richard Greene <richard.greene@autodesk.com>
2014-05-23 08:32:19 -0800

    improved formatting of status info
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 95166]

--- gem/php/UI.php
+++ gem/php/UI.php
@@ -19,7 +19,9 @@
             }
             fclose($handle);
 
+            echo "<p><font size='20'>Printer status: ";
             echo $timeLeft;
+            echo "</p>";
         }
 	}
 ?>
@@ -32,10 +34,7 @@
 </style>
 
 </head>
-<!-- <body  onload="GetStatus()"> -->
-<p><font size="20">Printer state or time remaining: <span id="timeLeft"></span></p>
 <div style="width: 800px; margin: 0px auto;">
-    <p><font size="20">Commands:</font></p>
     <button type="button" onclick="location.href='UI.php?cmd=Cancel'">CANCEL</button>
     <button type="button" onclick="location.href='UI.php?cmd=Reset'">RESET</button>
     <button type="button" onclick="location.href='UI.php?cmd=Pause'">PAUSE</button>
