4382bed447205ccb002c29ace104b65756ea1f35
Richard Greene <richard.greene@autodesk.com>
2014-05-22 18:02:31 -0800

    commands working again, with longer text strings
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 95152]

--- gem/php/UI.php
+++ gem/php/UI.php
@@ -2,32 +2,14 @@
 <head>
 
 
-<script>
- function GetStatus() {
-   // make url unique so IE doesn't return cached copy
-   url = "status.php?ms=" + new Date().getTime();
-
-   var xmlhttp=new XMLHttpRequest();
-   if(xmlhttp) {
-     xmlhttp.abort();
-     xmlhttp.onreadystatechange=function() {
-       if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-         document.getElementById("timeLeft").innerHTML=xmlhttp.responseText;
-       }  
-       setTimeout("GetStatus()", 2000);
-     }
-     xmlhttp.open("GET", url, true);
-     // header needed for lighttpd
-     xmlhttp.setRequestHeader("Accept", "*/*");
-     xmlhttp.send(null); 
-   }
- }
-
- function SendCommand(cmd) {
-    file_put_contents('/tmp/WebCommandPipe', cmd, FILE_APPEND | LOCK_EX);
- }
-
- </script>
+<?php
+	if (isset($_GET['cmd']))
+	{
+		$cmd = $_GET['cmd'];
+		
+        file_put_contents('/tmp/CommandPipe', "$cmd\n", FILE_APPEND | LOCK_EX);
+	}
+?>
 
 
 <title>Smith Command Input</title>
@@ -41,9 +23,9 @@
 <p><font size="20">Printer state or time remaining: <span id="timeLeft"></span></p>
 <div style="width: 800px; margin: 0px auto;">
     <p><font size="20">Commands:</font></p>
-    <button type="button" onclick="SendCommand('Cancel')">CANCEL</button>
-    <button type="button" onclick="SendCommand('Reset')">RESET</button>
-    <button type="button" onclick="SendCommand('Pause')">PAUSE</button>
-    <button type="button" onclick="SendCommand('Resume')">RESUME</button>
+    <button type="button" onclick="location.href='UI.php?cmd=Cancel'">CANCEL</button>
+    <button type="button" onclick="location.href='UI.php?cmd=Reset'">RESET</button>
+    <button type="button" onclick="location.href='UI.php?cmd=Pause'">PAUSE</button>
+    <button type="button" onclick="location.href='UI.php?cmd=Resume'">RESUME</button>
 </div>
 </html>
