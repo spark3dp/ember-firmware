6748747b3a4c875c55c469bdeafb59384c191056
Richard Greene <richard.greene@autodesk.com>
2014-05-23 18:48:24 -0800

    improved display of JSON status
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 95174]

--- gem/php/UI.php
+++ gem/php/UI.php
@@ -1,6 +1,27 @@
 <html>
 <head>
 
+<script type="text/javascript">
+
+function GetStatus()
+{
+    location.href = "UI.php?cmd=GetStatus";
+}
+
+</script>
+
+<title>Smith Command Input</title>
+<style type="text/css">
+	p { display: grid; }
+	button { font-size: 50px; width: 300px; margin: 10px; padding: 20px 20px;}
+</style>
+
+</head>
+<body onload="setTimeout('GetStatus();', 5000)">
+
+<table border="2">
+<tr>
+<td rowspan="5" width="800">
 
 <?php
 	if (isset($_GET['cmd']))
@@ -11,47 +32,45 @@
 
         if($cmd == "GetStatus")
         {
+            echo "<p><font size='10'>Printer status:</p>";
             // read from the status pipe & echo it to screen
-            $timeLeft = "NA";
             $handle = @fopen("/tmp/StatusToWebPipe", "r");
             if ($handle) {
-                $timeLeft = fgets($handle, 4096);
-                $timeLeft .= fgets($handle, 4096);
-                $timeLeft .= fgets($handle, 4096);
-                $timeLeft .= fgets($handle, 4096);
+                for($i = 0; $i < 12; $i += 1)
+                {
+                    echo "<p><font size='10'>";
+                    echo fgets($handle, 4096);
+                    echo "</p>";
+                }
             }
             fclose($handle);
-
-            echo "<p><font size='20'>Printer status: ";
-            echo $timeLeft;
-            echo "</p>";
         }
 	}
 ?>
 
-<script type="text/javascript">
+</td>
+<td><button type="button" onclick="location.href='UI.php?cmd=Cancel'">Cancel</button></td>
+</tr>
 
-function GetStatus()
-{
-    location.href = "UI.php?cmd=GetStatus";
-}
+<tr>
+<td><button type="button" onclick="location.href='UI.php?cmd=Reset'">Reset</button></td>
+</tr>
 
-</script>
+<tr>
+<td><button type="button" onclick="location.href='UI.php?cmd=Pause'">Pause</button></td>
+</tr>
+
+<tr>
+<td><button type="button" onclick="location.href='UI.php?cmd=Resume'">Resume</button></td>
+</tr>
+
+<tr>
+<td><button type="button" onclick="location.href='UI.php?cmd=GetStatus'">Get Status</button></td>
+</tr>
 
 
-<title>Smith Command Input</title>
-<style type="text/css">
-	p { display: table-cell; }
-	button { font-size: 80px; width: 800px; margin: 10px; padding: 20px 20px;}
-</style>
 
-</head>
-<body onload="setTimeout('GetStatus();', 10000)">
-<div style="width: 800px; margin: 0px auto;">
-    <button type="button" onclick="location.href='UI.php?cmd=Cancel'">CANCEL</button>
-    <button type="button" onclick="location.href='UI.php?cmd=Reset'">RESET</button>
-    <button type="button" onclick="location.href='UI.php?cmd=Pause'">PAUSE</button>
-    <button type="button" onclick="location.href='UI.php?cmd=Resume'">RESUME</button>
-    <button type="button" onclick="location.href='UI.php?cmd=GetStatus'">Get Status</button>
-</div>
+
+
+</table>
 </html>
