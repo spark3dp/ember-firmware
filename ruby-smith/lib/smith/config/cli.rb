a0836eea4058a201addc4fc7c7d5ae50d93c61fe
Jason Lefley <jason.lefley@autodesk.com>
2014-06-05 13:19:44 -0800

    Update config cli
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 95442]

--- ruby-smith/lib/smith/config/cli.rb
+++ ruby-smith/lib/smith/config/cli.rb
@@ -4,13 +4,13 @@ module Smith
   module Config
     class CLI < Thor
 
-      desc 'Load FILE', 'Load configuration from FILE'
+      desc 'load FILE', 'Load configuration from FILE'
       def load(file)
         Network.configure(YAML.load_file(file))
       end
 
-      desc 'set MODE', 'Configure the wireless adapter to operate in managed or adhoc MODE'
-      def set(mode)
+      desc 'mode MODE', 'Configure the wireless adapter to operate in managed or adhoc MODE'
+      def mode(mode)
         case mode
         when 'managed'
           Wireless.enable_managed_mode
