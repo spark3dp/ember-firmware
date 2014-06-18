bf26c272f4c793f60d953f9c89da2d12917f4b69
Richard Greene <richard.greene@autodesk.com>
2014-06-17 16:19:52 -0800

    added testing of default values
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 95675]

--- C++/tests/SettingsUT.cpp
+++ C++/tests/SettingsUT.cpp
@@ -19,14 +19,6 @@ void test1() {
     
     Settings settings;
     
-        // TODO
-    // try loading a file that doesn't exist
-   // settings.Load("UTSettings.json");
-    
-
-    // validate that settings have their default values
-    // settings.GetJobName();
-    
     // set some setting values
     settings.SetJobName("WhosYerDaddy");
     settings.SetLayerThicknessMicrons(42);
@@ -38,26 +30,26 @@ void test1() {
     
     // create a new instance of settings and verify initial default values
     Settings newSettings;
-//    if(newSettings.GetJobName().compare("") != 0)
-//    {
-//        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default job name: " 
-//                << newSettings.GetJobName() << std::endl;
-//    }
-//    if(newSettings.GetLayerThicknessMicrons() != 25)
-//    {
-//        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default layer thickness: " 
-//                << newSettings.GetLayerThicknessMicrons() << std::endl;
-//    }
-//    if(newSettings.GetModelExposureTimeSec() != 2.55)
-//    {
-//        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default model exposure time: " 
-//                << newSettings.GetModelExposureTimeSec() << std::endl;
-//    }
-//    if(newSettings.GetIsRegistered() != false)
-//    {
-//        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default isRegistered:  true " 
-//                 << std::endl;
-//    }    
+    if(newSettings.GetJobName().compare("") != 0)
+    {
+        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default job name: " 
+                << newSettings.GetJobName() << std::endl;
+    }
+    if(newSettings.GetLayerThicknessMicrons() != 25)
+    {
+        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default layer thickness: " 
+                << newSettings.GetLayerThicknessMicrons() << std::endl;
+    }
+    if(newSettings.GetModelExposureTimeSec() != 2.5)
+    {
+        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default model exposure time: " 
+                << newSettings.GetModelExposureTimeSec() << std::endl;
+    }
+    if(newSettings.GetIsRegistered() != false)
+    {
+        std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong default isRegistered:  true " 
+                 << std::endl;
+    }    
     
     // load new settings from the file and verify expected values
     newSettings.Load("/tmp/MySettings.json");   
@@ -82,6 +74,15 @@ void test1() {
         std::cout << "%TEST_FAILED% time=0 testname=test1 (SettingsUT) message=wrong new isRegistered: false" 
                   << std::endl;
     }
+    
+   
+   // TODO
+    Settings t2;
+   // try loading a file that doesn't exist
+   // t2.Load("UTSettings.json");
+    
+    // validate that settings have their default values
+    // t2.GetJobName() ...
 }
 
 int main(int argc, char** argv) {
