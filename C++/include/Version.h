a9a2278491e904165c2e8389c192c957a523b0c7
Richard Greene <richard.greene@autodesk.com>
2014-07-24 13:23:35 -0800

    adding file containing current firmware version
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 96447]
    
    
    Former-commit-id: c30d219be5fca5446661255b38e95f9bdf0871d2

---
+++ C++/include/Version.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   Version.h
+ * Author: Richard Greene
+ * 
+ *Defines the version number for the firmware.
+ * 
+ * Created on July 23, 2014, 11:12 AM
+ */
+
+#ifndef VERSION_H
+#define	VERSION_H
+
+#define FIRMWARE_VERSION ("0.1.0723.3\n")
+
+#endif	/* VERSION_H */
+
