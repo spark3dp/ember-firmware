7c23c16f3a0245d01de89df6c1848b137b2d301f
Jason Lefley <jason.lefley@autodesk.com>
2014-07-24 13:28:43 -0800

    Update definition importer to read version from correct header.  Update version.
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 96448]
    
    
    Former-commit-id: 2b4f928ddeb39856e74dc109eb2f72e314b143af

--- ruby/lib/smith/definitions.rb
+++ ruby/lib/smith/definitions.rb
@@ -3,5 +3,5 @@ module Smith
   STATE_PS_KEY = 'State'
   COMMAND_PIPE = '/tmp/CommandPipe'
   COMMAND_RESPONSE_PIPE = '/tmp/CommandResponsePipe'
-  FIRMWARE_VERSION = '0.1.0711.2\n'
+  FIRMWARE_VERSION = '0.1.0723.3\n'
 end
