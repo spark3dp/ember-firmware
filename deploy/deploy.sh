6a2e0ffb1ec4ed988188a387f4314453aa47b669
Jason Lefley <jason.lefley@autodesk.com>
2014-07-25 13:17:01 -0800

    Add firmware image deploy framework
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 96473]
    
    
    Former-commit-id: 9603eff5f7fc0cf2c805268d713620c00d87b7fa

---
+++ deploy/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ssh_opts="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet"
+scp ${ssh_opts} initramfs/init root@192.168.7.2:/usr/share/initramfs-tools/
+ssh ${ssh_opts} root@192.168.7.2 'bash -s' <<'ENDSSH'
+update-initramfs -u
+cp -v /boot/initrd.img-$(uname -r) /media/BEAGLE_BONE/initrd.img
+ENDSSH
