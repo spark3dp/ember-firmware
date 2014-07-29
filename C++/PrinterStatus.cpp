26d39df3d4a98487ee09affb9941b3f5677133b3
Richard Greene <richard.greene@autodesk.com>
2014-07-29 15:21:36 -0800

    adding (as part of starting to remove pointers from PrinterStatus)
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 96547]
    
    
    Former-commit-id: bdc247f8007d1a2220b0b16353e76019fc8aac23

---
+++ C++/PrinterStatus.cpp
@@ -0,0 +1,62 @@
+/* 
+ * File:   PrinterStatus.cpp
+ * Author: Richard Greene
+ * 
+ * The data structure used to communicate status from the print engine to UI 
+ * components.
+ *
+ * Created on July 28, 2014, 5:25 PM
+ */
+
+#include <PrinterStatus.h>
+#include <Logger.h>
+
+/// Constructor
+PrinterStatus::PrinterStatus() :
+_state(PrinterOnState),
+_change(NoChange),
+_UISubState(NoUISubState),
+_isError(false),
+_errorCode(0),
+_errorMessage(""),
+_numLayers(0),
+_currentLayer(0),
+_estimatedSecondsRemaining(0),
+_jobName(""),
+_temperature(0.0f)
+{
+}
+
+/// Gets the name of a print engine state machine state
+const char* PrinterStatus::StateName(PrintEngineState state)
+{
+    static bool initialized = false;
+    static const char* stateNames[MaxPrintEngineState];
+    if(!initialized)
+    {
+        // initialize the array of state names
+        stateNames[PrinterOnState] = "PrinterOn";
+        stateNames[DoorClosedState] = "DoorClosed";
+        stateNames[InitializingState] = "Initializing";
+        stateNames[DoorOpenState] = "DoorOpen";
+        stateNames[HomingState] = "Homing";
+        stateNames[HomeState] = "Home";
+        stateNames[IdleState] = "Idle";
+        stateNames[PrintSetupState] = "PrintSetup";
+        stateNames[MovingToStartPositionState] = "MovingToStartPosition";
+        stateNames[ExposingState] = "Exposing";
+        stateNames[PrintingState] = "Printing";
+        stateNames[PausedState] = "Paused";
+        stateNames[SeparatingState] = "Separating";
+        stateNames[EndingPrintState] = "EndingPrint";            
+        initialized = true;
+    }
+    
+    if(state <= UndefinedPrintEngineState ||
+       state >= MaxPrintEngineState)
+    {
+        LOGGER.HandleError(UNKNOWN_PRINT_ENGINE_STATE, false, NULL, state);
+        return "";                                                              
+    }
+    return stateNames[state];
+}
