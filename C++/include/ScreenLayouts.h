2489600c6b641c70f45a3177df6e1c99f43107ee
Richard Greene <richard.greene@autodesk.com>
2014-07-31 15:43:30 -0800

    includes remaining screen layouts for Smith alpha FW (to be used in inital 100-build)
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 96604]
    
    
    Former-commit-id: f92c1dc242a20442a6a081fe6db33166b8f7cfb9

--- C++/include/ScreenLayouts.h
+++ C++/include/ScreenLayouts.h
@@ -24,7 +24,7 @@
 #define	LOAD_FAIL_BTN2_LINE2        Right,      127,    112,    1, 	0xFFFF,     "Cancel"
 #define	LOAD_FAIL_LED_SEQ   4
 
-#define	PRINTING_LINE1              Center,     64,     16,     1, 	0xFFFF,     "Printing"
+#define	PRINTING_LINE1              Center,     64,     16,     1, 	0xFFFF,     "Printing."
 #define	PRINTING_LINE2              Center,     64,     32,     2, 	0xFFFF,     "%d:%02d"
 #define	PRINTING_LINE3              Center,     64,     68,     1, 	0xFFFF,     "remaining."
 #define	PRINTING_BTN1_LINE2         Left,       0,      112,    1, 	0xFFFF,     "Pause"
@@ -47,12 +47,52 @@
 #define	PRINT_COMPLETE_LINE1        Center,     64,     32,     1, 	0xFFFF,     "Print complete."
 #define	PRINT_COMPLETE_LINE2        Center,     64,     48,     1, 	0xFFFF,     "Clear the build"
 #define	PRINT_COMPLETE_LINE3        Center,     64,     64,     1, 	0xFFFF,     "area."
-#define	PRINT_COMPLETE_BTN1_LINE2   Left,       0,      112,    1, 	0xFFFF,     "Home"
+#define	PRINT_COMPLETE_BTN1_LINE2   Right,      127,    112,    1, 	0xFFFF,     "Home"
 #define	PRINT_COMPLETE_LED_SEQ  6
 
-#define    STARTING_PRINT_LINE1      Center,    64,     64,     1,   0xFFFF,     "Starting print..."
-#define    STARTING_PRINT_BTN2_LINE2 Right,     127,    112,    1,   0xFFFF,     "Cancel"
-#define    STARTING_PRINT_LED_SEQ    0
+#define STARTING_PRINT_LINE1        Center,     64,     64,     1,  0xFFFF,     "Starting print..."
+#define STARTING_PRINT_BTN2_LINE2   Right,      127,    112,    1,  0xFFFF,     "Cancel"
+#define STARTING_PRINT_LED_SEQ   0
 
+#define LOAD_FIRST_LINE1            Center,     64,     32,     1,  0xFFFF,     "Ready."
+#define LOAD_FIRST_LINE2            Center,     64,     48,     1,  0xFFFF,     "Load your prepped"
+#define LOAD_FIRST_LINE3            Center,     64,     64,     1,  0xFFFF,     "file through Spark"
+#define LOAD_FIRST_LINE4            Center,     64,     80,     1,  0xFFFF,     "or via USB." 
+#define LOAD_FIRST_LED_SEQ    0
 
+#define LOADING_FILE_LINE1          Center,     64,     64,     1,  0xFFFF,     "Loading file..."
+#define LOADING_FILE_BTN2_LINE2     Right,      127,    112,    1,  0xFFFF,     "Cancel"
+#define LOADING_FILE_LED_SEQ    0
+
+#define	CANCELED_LINE1              Center,     64,     32,     1, 	0xFFFF,     "Print canceled."
+#define	CANCELED_LINE2              Center,     64,     48,     1, 	0xFFFF,     "Clear the build"
+#define	CANCELED_LINE3              Center,     64,     64,     1, 	0xFFFF,     "area."
+#define	CANCELED_BTN2_LINE2         Right,      127,    112,    1, 	0xFFFF,     "Home"
+#define	CANCELED_LED_SEQ      7
+ 
+#define	DOOR_OPEN_LINE1             Center,     64,     16,     1, 	0xFFFF,     "Door open."
+#define	DOOR_OPEN_LINE2             Center,     64,     32,     1, 	0xFFFF,     "Please close the"
+#define	DOOR_OPEN_LINE3             Center,     64,     48,     1, 	0xFFFF,     "door ASAP to avoid"
+#define	DOOR_OPEN_LINE4             Center,     64,     64,     1, 	0xFFFF,     "exposing the photo-"
+#define	DOOR_OPEN_LINE5             Center,     64,     80,     1, 	0xFFFF,     "sensitive resin."
+#define	DOOR_OPEN_LED_SEQ    4
+
+#define	ERROR_CODE_LINE1            Center,     64,     32,     1, 	0xFFFF,     "{Error code}"
+#define	ERROR_CODE_LINE2            Center,     64,     48,     1, 	0xFFFF,     "Return home or"
+#define	ERROR_CODE_LINE3            Center,     64,     64,     1, 	0xFFFF,     "reset?"
+#define	ERROR_CODE_BTN1_LINE2       Right,      127,    112,    1, 	0xFFFF,     "Home"
+#define ERROR_CODE_BTN2_LINE2       Left,       0,      112,    1,  0xFFFF,     "Reset"
+#define	ERROR_CODE_LED_SEQ  4
+
+#define	ERROR_DURING_PRINT_LINE1    Center,     64,     16,     1, 	0xFFFF,     "An error occurred"
+#define	ERROR_DURING_PRINT_LINE2    Center,     64,     32,     1, 	0xFFFF,     "while printing."
+#define	ERROR_DURING_PRINT_LINE3    Center,     64,     48,     1, 	0xFFFF,     "Confirm the build"
+#define	ERROR_DURING_PRINT_LINE4    Center,     64,     64,     1, 	0xFFFF,     "area is clear before"
+#define	ERROR_DURING_PRINT_LINE5    Center,     64,     80,     1, 	0xFFFF,     "proceeding."
+#define	ERROR_DURING_PRINT_BTN1_LINE2 Right,    127,    112,    1, 	0xFFFF,     "Home"
+#define	ERROR_DURING_PRINT_LED_SEQ    4
+
+#define	HOMING_LINE1                Center,     64,     48,     1, 	0xFFFF,     "Homing the build"
+#define	HOMING_LINE2                Center,     64,     64,     1, 	0xFFFF,     "platform. Please wait..."
+#define	HOMING_LED_SEQ  0
 
