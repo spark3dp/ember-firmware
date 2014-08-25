2431a8f9e846b397abcb40c33c468dcaed2d6f73
Jason Lefley <jason.lefley@autodesk.com>
2014-08-25 12:30:13 -0800

    Update gem install commands
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 97086]
    
    
    Former-commit-id: e6ef5b7de51e0077944dd3380e70740b6524f1ac

--- deploy/build_image_scripts/install.sh
+++ deploy/build_image_scripts/install.sh
@@ -11,8 +11,8 @@ cp -v /smith/smith "${FS_ROOT}/usr/local/bin"
 
 echo 'Installing smith gem'
 # Uninstall any existing gems
-chroot "${FS_ROOT}" gem list | cut -d" " -f1 | chroot "${FS_ROOT}" xargs gem uninstall -aIx > /dev/null 2>&1 || true 
-# Get gem installation directory
-gem_home="${FS_ROOT}$(chroot "${FS_ROOT}" gem environment | grep -Po 'INSTALLATION DIRECTORY: \K[^"]*')"
+chroot "${FS_ROOT}" gem list | cut -d" " -f1 | chroot "${FS_ROOT}" xargs gem uninstall -aIx --bindir /usr/local/bin  > /dev/null 2>&1 || true
+# Get gem installation path
+gem_dir="${FS_ROOT}$(chroot "${FS_ROOT}" gem environment | grep -Po 'INSTALLATION DIRECTORY: \K[^"]*')"
 # Install smith gem
-(cd /root/cache && GEM_HOME="${gem_home}" GEM_PATH="${gem_home}" gem install smith --local --no-ri --no-rdoc --bindir "${FS_ROOT}/usr/local/bin")
+(cd /root/cache && GEM_HOME="${gem_dir}" GEM_PATH="${gem_dir}" gem install smith --local --no-ri --no-rdoc --install-dir "${gem_dir}" --bindir "${FS_ROOT}/usr/local/bin")
