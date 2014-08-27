59afaf3972ab2a3ca6512f29382faa70cc7fb64b
Jason Lefley <jason.lefley@autodesk.com>
2014-08-27 13:18:42 -0800

    Import definitions/version bump
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 97148]
    
    
    Former-commit-id: 10a4047d07a983dd0ce525f0f120c58156dc617f

--- ruby/lib/smith/definitions.rb
+++ ruby/lib/smith/definitions.rb
@@ -4,5 +4,5 @@ module Smith
   UISUBSTATE_PS_KEY = 'UISubState'
   COMMAND_PIPE = '/tmp/CommandPipe'
   COMMAND_RESPONSE_PIPE = '/tmp/CommandResponsePipe'
-  FIRMWARE_VERSION = '0.2.0825.0\n'
+  FIRMWARE_VERSION = '0.2.0827.0\n'
 end
