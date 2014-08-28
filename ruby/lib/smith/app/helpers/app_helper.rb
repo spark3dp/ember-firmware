3fb49a6ae4fa33014cc310a59bbeae6bc67b9c8c
Jason Lefley <jason.lefley@autodesk.com>
2014-08-28 13:59:52 -0800

    Add helper method
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 97172]
    
    
    Former-commit-id: 384a609081c44b542dfd0aafebdc8b479db69819

--- ruby/lib/smith/app/helpers/app_helper.rb
+++ ruby/lib/smith/app/helpers/app_helper.rb
@@ -14,6 +14,17 @@ module Smith
         fields.join("\n")
       end
 
+      # Clear the flash and return a json string of the contents
+      def flash_json
+        msg = Hash[flash.keys.map { |k| [k, flash[k]] }]
+        # Clear the flash values
+        flash.flag!
+        flash.sweep!
+        # Clear the flash cache
+        flash.now.clear
+        msg.to_json
+      end
+
     end
   end
 end
