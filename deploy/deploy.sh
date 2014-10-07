fe684f97b89e2b73036692732811d9ddb6d671e7
Jason Lefley <jason.lefley@autodesk.com>
2014-10-07 11:14:15 -0800

    specify username
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 97684]
    
    
    Former-commit-id: 3eabaa89715434a80725102a32ced87c278004a2

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-rsync -auv --delete --exclude=deploy/setup/main/firmware --exclude=deploy/deploy --exclude=deploy/git --exclude=oib.log --exclude=.*.swp "${SCRIPTPATH}" 192.168.7.2:~/
+rsync -auv --delete --exclude=deploy/setup/main/firmware --exclude=deploy/deploy --exclude=deploy/git --exclude=oib.log --exclude=.*.swp "${SCRIPTPATH}" root@192.168.7.2:~/
