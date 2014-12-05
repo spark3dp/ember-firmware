da5bff02454b320ab3272159bac1700b0c065b9a
Richard Greene <richard.greene@autodesk.com>
2014-12-05 09:45:33 -0800

    adding project for testing front panel during manufacturing
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 98364]
    
    
    Former-commit-id: 703e6a78e4f63748ba5645771b1f45490120f45d

---
+++ manufacturing_support/FPTest/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * 
+ * For testing Ember front panel during manufacturing.
+ * 
+ * Author: Richard Greene
+ *
+ * Created on December 5, 2014, 9:39 AM
+ */
+
+#include <I2C_Device.h>
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
