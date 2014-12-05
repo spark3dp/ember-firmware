36516042309144ed53096e05db08ab0bd9554cb8
Richard Greene <richard.greene@autodesk.com>
2014-12-05 11:20:29 -0800

    shows promt for first button presses
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 98366]
    
    
    Former-commit-id: 785b5112e5a663db171135275b09cab4e6f90bc5

--- manufacturing_support/FPTest/main.cpp
+++ manufacturing_support/FPTest/main.cpp
@@ -13,13 +13,38 @@
 #include <cstdlib>
 #include <stdio.h>
 #include <string.h>
-
+#include <string>
 
 using namespace std;
 
-/*
- * 
- */
+/// Show one line of text on the OLED display, using its location, alignment, 
+/// size, and color.
+void ShowText(I2C_Device* frontPanel, unsigned char align, unsigned char x, unsigned char y, 
+                          unsigned char size, int color, std::string text)
+{
+    unsigned char cmd = align;
+    
+    int textLen = text.length();
+    if(textLen > MAX_OLED_STRING_LEN)
+    {
+        // truncate text to prevent overrunning the front panel's I2C buffer 
+        textLen = MAX_OLED_STRING_LEN;
+    }
+    
+    // the command structure is:
+    // [CMD_START][FRAME LENGTH][CMD_OLED][CMD_OLED_xxxTEXT][X BYTE][Y BYTE]
+    // [SIZE BYTE] [HI COLOR BYTE][LO COLOR BYTE][TEXT LENGTH][TEXT BYTES] ...
+    // [CMD_END]
+    unsigned char cmdBuf[35] = 
+        {CMD_START, 8 + textLen, CMD_OLED, cmd, x, y, size, 
+         (unsigned char)((color & 0xFF00) >> 8), (unsigned char)(color & 0xFF), 
+         textLen};
+    memcpy(cmdBuf + 10, text.c_str(), textLen);
+    cmdBuf[10 + textLen] = CMD_END;
+    
+    frontPanel->Write(UI_COMMAND, cmdBuf, 11 + textLen);
+}
+
 int main(int argc, char** argv) {
 
     int port = I2C2_PORT;
@@ -32,9 +57,14 @@ int main(int argc, char** argv) {
  
     I2C_Device frontPanel(UI_SLAVE_ADDRESS, port);
     
+    // light all LEDs and fade them up and down
     const unsigned char ledSequence[] = {CMD_START, 3, CMD_RING, CMD_RING_SEQUENCE, 8, CMD_END};
     frontPanel.Write(UI_COMMAND, ledSequence, strlen((const char*)ledSequence));
     
+    // show a prompt
+    ShowText(&frontPanel, CMD_OLED_CENTERTEXT, 64,  8, 1, 0xFFFF, "Press button twice");
+    ShowText(&frontPanel, CMD_OLED_CENTERTEXT, 64, 112, 1, 0xFFFF, "to clear the display.");
+   
     return 0;
 }
 
