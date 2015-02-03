eb4709ac899a6f01a0cf5bd51bc09d4e02555961
Jason Lefley <jason.lefley@autodesk.com>
2015-02-03 14:16:28 -0800

    Update script to use original versioning scheme
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 98796]
    
    
    Former-commit-id: 4149aa3fd4adfbffbe6b293dc8fc9e179c36fbe9

--- infrastructure/update_version.rb
+++ infrastructure/update_version.rb
@@ -1,27 +1,50 @@
 #!/usr/bin/env ruby
 
-# Updates FIRMWARE_VERSION definition in specified file
+# Updates BUILD_DATE and BUILD_NUMBER definition in specified file
 # Writes version.properties file for injection of firmware version into Bamboo
 
-MAJOR_MINOR_REGEX = /#define FIRMWARE_VERSION "(.*?)"/
+DATE_REGEX = /#define BUILD_DATE "(.*?)"/
+NUMBER_REGEX = /#define BUILD_NUMBER "(.*?)"/
+MAJOR_REGEX = /#define VERSION_MAJOR "(.*?)"/
+MINOR_REGEX = /#define VERSION_MINOR "(.*?)"/
 
-if !(shared_header = ARGV[0])
-  abort('Path to shared header file to update must be specified as first argument')
+if !(build_h = ARGV[0])
+  abort('Path to Build.h must be specified as first argument')
 end
 
-if !(changelist = ARGV[1])
-  abort('Changelist must be specified as second argument')
+if !(shared_h= ARGV[1])
+  abort('Path to Shared.h must be specified as second argument')
 end
 
-header_contents = File.read(shared_header)
+build_h_contents = File.read(build_h)
+shared_h_contents = File.read(shared_h)
 
-if !(match_groups = header_contents.match(MAJOR_MINOR_REGEX))
-  abort('Unable to match existing #define in shared header file')
+date_match_groups = build_h_contents.match(DATE_REGEX)
+number_match_groups = build_h_contents.match(NUMBER_REGEX)
+major_match_groups = shared_h_contents.match(MAJOR_REGEX)
+minor_match_groups = shared_h_contents.match(MINOR_REGEX)
+
+if !date_match_groups || !number_match_groups || !major_match_groups || !minor_match_groups
+  abort('Unable to match existing preprocessor definitions in specified header files')
 end
 
-new_version = "#{match_groups[1]}.#{changelist}"
-new_contents = header_contents.sub(/#define FIRMWARE_VERSION ".*?"/, "#define FIRMWARE_VERSION \"#{new_version}\"")
+current_build_date = date_match_groups[1]
+current_build_number = number_match_groups[1]
+version_major = major_match_groups[1]
+version_minor = minor_match_groups[1]
 
-File.write(shared_header, new_contents)
-File.write('version.properties', "firmwareVersion=#{new_version}")
+build_date = Time.now.strftime('%Y%m%d')
+
+if build_date == current_build_date
+  build_number = ((current_build_number.to_i) + 1).to_s
+else
+  build_number = '0'
+end
+
+new_contents = build_h_contents.
+  sub(DATE_REGEX, "#define BUILD_DATE \"#{build_date}\"").
+  sub(NUMBER_REGEX, "#define BUILD_NUMBER \"#{build_number}\"")
+
+File.write(build_h, new_contents)
+File.write('version.properties', "firmwareVersion=#{version_major}.#{version_minor}.#{build_date}.#{build_number}")
 
