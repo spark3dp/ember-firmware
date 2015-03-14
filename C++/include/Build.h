3487c51b7c5ba633fb51829232cfd69624042e62
ACG Build Bot (123D-ACG-Hickory_write) <Dinu.Bunduchi@autodesk.com>
2015-03-13 23:12:24 -0800

    Automated submission from build machine: update version number to 1.2.20150314.0 [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 99096]
    Former-commit-id: 2e72133353dbc4e4ca2f009ce88d88a245d07c83

--- C++/include/Build.h
+++ C++/include/Build.h
@@ -12,9 +12,9 @@
 #define	BUILD_H
 
 // Date of build
-#define BUILD_DATE "20150312"
+#define BUILD_DATE "20150314"
 
 // Build attempt on BUILD_DATE
-#define BUILD_NUMBER "1"
+#define BUILD_NUMBER "0"
 
 #endif	/* BUILD_H */
