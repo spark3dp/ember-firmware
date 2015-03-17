1b3fae9b3cbe9b853f434845a3ed39f2c9f6fcca
Richard Greene <richard.greene@autodesk.com>
2015-03-17 09:29:48 -0800

    adding method to generate Spark-compliant command acknowledge payload
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 99115]
    
    
    Former-commit-id: 9328ebc8a865db0ca285079a5bc289d0df8df3c6

--- ruby/lib/smith/client/payload_helper.rb
+++ ruby/lib/smith/client/payload_helper.rb
@@ -11,25 +11,49 @@ module Smith
             error_code: status[ERROR_CODE_PS_KEY],
             error_message: status[ERROR_MSG_PS_KEY],
             data: status
-        }
-        if !status[SPARK_JOB_STATE].empty?
-          job_id = if status[JOB_ID_PS_KEY].empty?
-            'local'
-          else
-            status[JOB_ID_PS_KEY]
-          end
-          job_progress = if status[TOAL_LAYERS_PS_KEY] == 0
-            0.0
-          else
-            status[LAYER_PS_KEY].to_f / status[TOAL_LAYERS_PS_KEY].to_f
-          end
-          payload.merge!(job_id: job_id, job_status: status[SPARK_JOB_STATE], job_progress: job_progress)
-        end
-        return payload
+        }.merge!(job_specific_payload(status))
       end
 
-      def command_payload(status)
+      def command_payload(command, command_state, message, status)
+        payload = {
+            printer_status: status[SPARK_STATE],
+            progress:   if command_state == Command::RECEIVED_ACK
+                          0
+                        else
+                          1
+                        end,
+            error_code: if command_state == Command::FAILED_ACK
+                          500
+                        else
+                          0
+                        end,
+            error_message: message,
+            data: {
+                command: command,
+                message: message,
+                state: command_state
+            }
+        }.merge!(job_specific_payload(status))
+      end
 
+      def job_specific_payload(status)
+        # non-empty spark job state indicates there is a job in progress,
+        # or at least printable data available for a "local" job
+        if !status[SPARK_JOB_STATE].empty?
+          job_id = if status[JOB_ID_PS_KEY].empty?
+                     'local'
+                   else
+                     status[JOB_ID_PS_KEY]
+                   end
+          job_progress = if status[TOAL_LAYERS_PS_KEY] == 0
+                           0.0
+                         else
+                           status[LAYER_PS_KEY].to_f / status[TOAL_LAYERS_PS_KEY].to_f
+                         end
+          return { job_id: job_id, job_status: status[SPARK_JOB_STATE], job_progress: job_progress }
+        else
+          return { }
+        end
       end
 
     end
