495b3cfba6b07d57d060a90d05b4002ebd598cd9
Jason Lefley <jason.lefley@autodesk.com>
2015-05-04 13:17:49 -0800

    Add max values, comment, and change register values to be sequential and start at 1
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 99551]
    
    
    Former-commit-id: 6d3c02adf1684bad9b720c91e09b2189caad81e0

--- C++/include/MotorController.h
+++ C++/include/MotorController.h
@@ -10,15 +10,20 @@
 #ifndef MOTORCONTROLLER_H
 #define	MOTORCONTROLLER_H
 
+// the first value in each group needs to be 1
+// high fencepost values need to be equal to one greater than the last
+// code in the group
+
 // command (write-only) register addresses 
-#define MC_GENERAL_REG      (0x20) // for general motor controller commands 
-#define MC_ROT_SETTINGS_REG (0x21) // for rotation settings  
-#define MC_ROT_ACTION_REG   (0x22) // for rotation actions 
-#define MC_Z_SETTINGS_REG   (0x23) // for Z axis settings  
-#define MC_Z_ACTION_REG     (0x24) // for Z axis actions
+#define MC_GENERAL_REG        (0x01) // for general motor controller commands 
+#define MC_ROT_SETTINGS_REG   (0x02) // for rotation settings  
+#define MC_ROT_ACTION_REG     (0x03) // for rotation actions 
+#define MC_Z_SETTINGS_REG     (0x04) // for Z axis settings  
+#define MC_Z_ACTION_REG       (0x05) // for Z axis actions
 
 // status (read-only) register address 
-#define MC_STATUS           (0x30) // gives motor controller status
+#define MC_STATUS             (0x06) // gives motor controller status
+#define MC_REG_HIGH_FENCEPOST (0x07)
 
 // general motor controller commands (with no argument)
 #define MC_INTERRUPT        (1) // generate an interrupt
@@ -26,6 +31,7 @@
 #define MC_CLEAR            (3) // clear all pending commands 
 #define MC_PAUSE            (4) // pause the command in progress 
 #define MC_RESUME           (5) // when paused, resume all pending command(s)  
+#define MC_GENERAL_HIGH_FENCEPOST (6)
 
 // settings commands, for either rotation or Z axis (with int argument, x)
 #define MC_STEP_ANGLE       (1) // set number of degrees/1000 for each step
@@ -38,6 +44,7 @@
                                 // units/minute^3/1E6
 #define MC_SPEED            (5) // set speed for move (units/minute)
 #define MC_MAX_SPEED        (6) // set max speed overall (units/minute)
+#define MC_SETTINGS_HIGH_FENCEPOST (7)
 
 
 // action command, for either rotation or Z axis (with int argument, x)
@@ -49,6 +56,7 @@
 // action commands, for either rotation or Z axis (with no argument)
 #define MC_ENABLE           (3) // enable the motor
 #define MC_DISABLE          (4) // disable the motor 
+#define MC_ACTION_HIGH_FENCEPOST (5)
 
 // status codes
 #define MC_SUCCESS          (0)
