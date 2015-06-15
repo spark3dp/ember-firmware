a12872412d466376bb6bd28a48a9c259eb9ce4f0
Jason Lefley <jason.lefley@autodesk.com>
2015-06-15 14:15:55 -0800

    Add missing file
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 99840]
    
    
    Former-commit-id: 7773211ed1e8163a4c9c8f904b148c484931f489

---
+++ AVR/MotorController/Planner.h
@@ -0,0 +1,25 @@
+/*
+ * Planner.h
+ * Author: Jason Lefley
+ * Date  : 2015-06-14
+ */
+
+#ifndef PLANNER_H
+#define PLANNER_H
+
+#include "MotorController.h"
+#include "Status.h"
+
+namespace Planner
+{
+void Initialize(MotorController_t* mcState);
+void SetAxisPosition(uint8_t axis, const float position);
+void SetPulsesPerUnit(uint8_t axis, float value);
+Status PlanAccelerationLine(const float distances[], const uint8_t directions[], float speed, float maxSpeed);
+Status PlanHoldCallback(void);
+void BeginHold();
+Status EndHold(void);
+void EndMove();
+}
+
+#endif /* PLANNER_H */
