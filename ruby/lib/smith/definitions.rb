e5f6af03f666e4723c92aaf4e90031bfa6e9e5bb
ACG Build Bot (123D-ACG-Hickory_write) <Dinu.Bunduchi@autodesk.com>
2015-06-19 23:00:10 -0800

    Automated submission from build machine: update version number to 2.0.20150620.0 [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 99875]
    Former-commit-id: 4c892d947e3b9f70f28d162bbbfdf3388d4e3067

--- ruby/lib/smith/definitions.rb
+++ ruby/lib/smith/definitions.rb
@@ -97,6 +97,6 @@ module Smith
   SETTINGS_ROOT_KEY = 'Settings'
   PRINT_FILE_SETTING = 'PrintFile'
   JOB_ID_SETTING = 'JobID'
-  BUILD_DATE = '20150619'
+  BUILD_DATE = '20150620'
   BUILD_NUMBER = '0'
 end
