3582f20468ed48d38ee59f6de610ca64738dffc9
Richard Greene <richard.greene@autodesk.com>
2015-07-13 19:59:56 -0800

    better centering of print progress screen
    [git-p4: depot-paths = "//depot/ACG/Smith/Firmware/": change = 99931]
    
    
    Former-commit-id: f7bcf8c294849a1d4b6a8bf23ff6abdf75cc756f

--- C++/include/ScreenLayouts.h
+++ C++/include/ScreenLayouts.h
@@ -40,11 +40,11 @@
 #define DOWNLOAD_FAIL_BTN2_LINE2    Right,      127,    112,    1,  0xFFFF,     "Okay"
 #define DOWNLOAD_FAIL_LED_SEQ   4
 
-#define PRINTING_LINE1              Center,     64,     0,      1,  0xFFFF,     "Printing"
-#define PRINTING_LINE2              Center,     64,     16,     1,  0xFFFF,     "%s" // job name
-#define PRINTING_CLEAR_LINE3        Center,     64,     33,     2,  0x0000,     "%s" // clears previous time
-#define PRINTING_LINE3              Center,     64,     33,     2,  0xFFFF,     "%s" // shows new time
-#define PRINTING_LINE4              Center,     64,     72,     1,  0xFFFF,     "remaining."
+#define PRINTING_LINE1              Center,     64,     8,      1,  0xFFFF,     "Printing"
+#define PRINTING_LINE2              Center,     64,     24,     1,  0xFFFF,     "%s" // job name
+#define PRINTING_CLEAR_LINE3        Center,     64,     46,     2,  0x0000,     "%s" // clears previous time
+#define PRINTING_LINE3              Center,     64,     46,     2,  0xFFFF,     "%s" // shows new time
+#define PRINTING_LINE4              Center,     64,     88,     1,  0xFFFF,     "remaining."
 #define PRINTING_BTN1_LINE2         Left,       0,      112,    1,  0xFFFF,     "Cancel"
 #define PRINTING_BTN2_LINE2         Right,      127,    112,    1,  0xFFFF,     "Pause"
 #define PRINTING_LED_SEQ    0
