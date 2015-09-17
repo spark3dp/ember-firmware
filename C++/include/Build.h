e930696492263373929a5f795281f3a35e5b1b9c
Automated build machine <jason.lefley@autodesk.com>
2015-09-17 00:00:19 -0700

    Automated commit from build machine: update version number to 2.2.20150917.0

--- C++/include/Build.h
+++ C++/include/Build.h
@@ -26,9 +26,9 @@
 #define	BUILD_H
 
 // Date of build
-#define BUILD_DATE "20150915"
+#define BUILD_DATE "20150917"
 
 // Build attempt on BUILD_DATE
-#define BUILD_NUMBER "1"
+#define BUILD_NUMBER "0"
 
 #endif    // BUILD_H
