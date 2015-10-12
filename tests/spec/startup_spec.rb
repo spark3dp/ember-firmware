8bbda984041caf66d23db1c5ffc484ac3b73ede4
Jason Lefley <jason.lefley@autodesk.com>
2015-10-12 13:35:56 -0700

    get motor controller assertions passing in startup spec

--- tests/spec/startup_spec.rb
+++ tests/spec/startup_spec.rb
@@ -1,6 +1,7 @@
 require 'open3'
 require 'fcntl'
 require 'mkfifo'
+require 'json'
 
 require 'spec_helper'
 require 'support/definitions'
@@ -34,6 +35,8 @@ module Tests
   end
 
   class MotorController
+
+    Motion = Struct.new(:axis, :action, :parameter, :settings)
 
     class Command
 
@@ -69,13 +72,15 @@ module Tests
         @received_command_count > 0
       end
 
+      def has_partial_command?
+        @bytes_remaining != COMMAND_SIZE
+      end
+
       # store the specified byte as a string in the buffer
       # storing as string allows the command to unpack the parameter bytes directly as a signed 32-bit integer
       def add(data)
         unpacked_data = data.unpack('C').first
 
-        # ignore read register address
-        return if unpacked_data == MC_STATUS_REG && @bytes_remaining == COMMAND_SIZE
 
         if unpacked_data > MC_GENERAL_LOW_FENCEPOST && unpacked_data < MC_GENERAL_HIGH_FENCEPOST &&
             @bytes_remaining == COMMAND_SIZE
@@ -126,46 +131,101 @@ module Tests
 
     end
 
-    class AxisState
+    class AxisSettings
+      attr_accessor :step_angle, :units_per_revolution, :microstepping_factor, :speed, :max_jerk
+
+      def initialize
+        reset
+      end
+
+      def reset
+        @step_angle = 0.0
+        @units_per_revolution = 0.0
+        @microstepping_factor = 0
+        @max_jerk = 0.0
+        @speed = 0.0
+      end
     end
 
-    attr_reader :r_axis, :z_axis
+    # i2c_write_pipe_path - path to the named pipe that the firmware writes I2C data to, the tests read from this pipe
+    # i2c_read_pipe_path - path to the named pipe that the firmware reads I2C data from, the tests write to this pipe
+    # interrupt_read_pipe_path - path to the named pipe that the firmware monitors for activity indicating that the
+    # motor controller has generated an interrupt, the tests write to this pipe
+    def initialize(i2c_write_pipe_path, i2c_read_pipe_path, interrupt_read_pipe_path)
+      File.mkfifo(i2c_write_pipe_path)      unless File.pipe?(i2c_write_pipe_path)
+      File.mkfifo(i2c_read_pipe_path)       unless File.pipe?(i2c_read_pipe_path)
+      File.mkfifo(interrupt_read_pipe_path) unless File.pipe?(interrupt_read_pipe_path)
 
-    def initialize(i2c_read_pipe_path, i2c_write_pipe_path, interrupt_write_pipe_path)
-      File.mkfifo(i2c_read_pipe_path)        unless File.pipe?(i2c_read_pipe_path)
-      File.mkfifo(i2c_write_pipe_path)       unless File.pipe?(i2c_write_pipe_path)
-      File.mkfifo(interrupt_write_pipe_path) unless File.pipe?(interrupt_write_pipe_path)
-      @i2c_read_pipe        = File.open(i2c_read_pipe_path, 'r+')
-      @i2c_write_pipe       = File.open(i2c_write_pipe_path, 'w+')
-      @interrupt_write_pipe = File.open(interrupt_write_pipe_path, 'w+')
-      @buffer = CommandBuffer.new
-      @status = MC_STATUS_SUCCESS
-      @r_axis = AxisState.new
-      @z_axis = AxisState.new
+      @i2c_write_pipe      = File.open(i2c_write_pipe_path, 'r+')
+      @i2c_read_pipe       = File.open(i2c_read_pipe_path, 'w+')
+      @interrupt_read_pipe = File.open(interrupt_read_pipe_path, 'w+')
+
+      @buffer = CommandBuffer.new         # buffer to hold commands as they are sent to the motor controller
+      @status = MC_STATUS_SUCCESS         # the current motor controller status
+      @r_axis_settings = AxisSettings.new # holds the current r axis settings
+      @z_axis_settings = AxisSettings.new # holds the current z axis settings
+      @enabled = false                    # flag recording whether or not the motor controller is enabled
+      @movements = []                     # array holding movement requests sent to motor controller
+      @interrupt_requests = 0             # number of interrupt requests sent to motor controller
     end
-
 
     def update
-      @buffer.add(@i2c_read_pipe.read_nonblock(1))
+      # read a single byte from the pipe that the firmware writes I2C data to
+      data = @i2c_write_pipe.read_nonblock(1)
+      unpacked_data = data.unpack('C').first
+
+      if unpacked_data == MC_STATUS_REG && !@buffer.has_partial_command?
+        # write status byte to pipe that firmware reads I2C data from
+        @i2c_read_pipe.write([@status].pack('C'))
+        @i2c_read_pipe.flush
+        # don't handle byte as part of multi-byte command message
+        return
+      end
+
+      @buffer.add(data)
+
       if @buffer.has_command?
         command = @buffer.command
-        puts "got command, register: #{command.register.to_s(16)}, action: #{command.action.to_s(16)}, parameter: #{command.parameter}"
+        #puts "got command, register: #{command.register.to_s(16)}, action: #{command.action.to_s(16)}, parameter: #{command.parameter}"
         case command.register
           when MC_GENERAL_REG
             general_command(command.action)
           when MC_ROT_SETTINGS_REG
-            settings_command(command.action, command.parameter, @r_axis)
+            settings_command(command.action, command.parameter, @r_axis_settings)
           when MC_ROT_ACTION_REG
-            action_command(command.action, command.parameter, @r_axis)
+            action_command(command.action, command.parameter, @r_axis_settings, :r)
           when MC_Z_SETTINGS_REG
-            settings_command(command.action, command.parameter, @z_axis)
+            settings_command(command.action, command.parameter, @z_axis_settings)
           when MC_Z_ACTION_REG
-            action_command(command.action, command.parameter, @z_axis)
+            action_command(command.action, command.parameter, @z_axis_settings, :z)
           else
             @status = MC_STATUS_COMMAND_UNKNOWN
         end
       end
     rescue IO::WaitReadable
+      # pipe did not contain data
+    end
+
+    def next_movement
+      @movements.shift
+    end
+
+    def enabled?
+      @enabled
+    end
+
+    def disabled?
+      !@enabled
+    end
+
+    def respond_to_interrupt_request
+      if @interrupt_requests > 0
+        @interrupt_read_pipe.write('1')
+        @interrupt_read_pipe.flush
+        @interrupt_requests -= 1
+      else
+        raise 'motor controller instructed to respond to interrupt request but did not receive interrupt request command from firmware'
+      end
     end
 
     private
@@ -173,32 +233,45 @@ module Tests
     def general_command(action)
       case action
         when MC_INTERRUPT
+          @interrupt_requests += 1
         when MC_RESET
+          @r_axis_settings.reset
+          @z_axis_settings.reset
         when MC_CLEAR
         when MC_PAUSE
         when MC_RESUME
         when MC_ENABLE
+          @enabled = true
         when MC_DISABLE
+          @enabled = false
         else
           @status = MC_STATUS_COMMAND_UNKNOWN
       end
     end
 
-    def settings_command(action, parameter, axis_state)
+    def settings_command(action, parameter, axis_settings)
       case action
         when MC_STEP_ANGLE
+          axis_settings.step_angle = parameter
         when MC_MICROSTEPPING
+          axis_settings.microstepping_factor = parameter
         when MC_JERK
+          axis_settings.max_jerk = parameter
         when MC_SPEED
+          axis_settings.speed = parameter
+        when MC_UNITS_PER_REV
+          axis_settings.units_per_revolution = parameter
         else
           @status = MC_STATUS_COMMAND_UNKNOWN
       end
     end
 
-    def action_command(action, parameter, axis_state)
+    def action_command(action, parameter, axis_settings, axis)
       case action
-        when MC_MOVE
-        when MC_HOME
+        when MC_MOVE, MC_HOME
+          # record the settings at the time of the action so the tests can make assertions regarding what settings the
+          # motor controller carried out the action with
+          @movements << Motion.new(axis, action, parameter, axis_settings.clone)
         else
           @status = MC_STATUS_COMMAND_UNKNOWN
       end
@@ -208,30 +281,17 @@ module Tests
 
   class Settings
 
-    def initialize
+    def initialize(path)
+      @settings = JSON.parse(File.read(path)).fetch(SETTINGS_ROOT_KEY)
     end
 
     def get(key)
+      @settings.fetch(key)
     end
 
   end
 
-  RSpec::Matchers.define :move_to do |expected|
-    match do |axis_state|
-      axis_state.position == expected
-    end
 
-    failure_message do |axis_state|
-      "expected #{axis_state.label} axis to be at #{expected}, axis actually at #{axis_state.position}"
-    end
-
-    chain :with_max_travel do |axis_state|
-      puts "expected: #{expected.inspect}"
-      puts "axis state: #{axis_state.inspect}"
-    end
-
-    chain :with_settings do ||
-  end
 
   describe 'firmware' do
     let(:smith) { Smith.new }
@@ -242,15 +302,12 @@ module Tests
         tmp_dir(MOTOR_CONTROLLER_INTERRUPT_READ_PIPE)
       )
     }
-    let(:settings) { Settings.new }
+    let(:settings) { Settings.new("#{ROOT_DIR}#{SETTINGS_SUB_DIR}#{SETTINGS_FILE}") }
     after(:each) { smith.stop }
 
-    scenario 'startup' do
-
-      smith.start(tmp_dir_path)
-
+    def poll
       count = 0
-      timeout_seconds = 1.0
+      timeout_seconds = 5.0
       poll_interval_seconds = 0.01
       max_count = timeout_seconds / poll_interval_seconds
       loop do
@@ -259,9 +316,15 @@ module Tests
         sleep poll_interval_seconds
         count += 1
       end
+    end
+
+    scenario 'startup' do
+
+      smith.start(tmp_dir_path)
+
+      poll
 
 =begin
-
       expect(front_panel).to display(
         'Ready.
         Load your prepped
@@ -270,26 +333,45 @@ module Tests
       )
 =end
 
-      #expect(motor_controller.r_axis).to move_home_with_max_travel(settings.get(R_HOMING_ANGLE)).with_settings(
-      expect(motor_controller.r_axis).to move_home.with_max_travel(123).with_settings(
-        step_angle: settings.get(R_STEP_ANGLE),
-        units_per_rev: settings.get(R_MILLIDEGREES_PER_REV),
-        microstepping: settings.get(MICRO_STEPS_MODE),
-        jerk: settings.get(R_HOMING_JERK),
-        speed: settings.get(R_HOMING_SPEED)
-      )
+      expect(motor_controller).to be_enabled
 
-      expect(motor_controller.z_axis).to move_home_with_max_travel(-2 * settings.get(Z_START_PRINT_POSITION)).with_settings(
-        step_angle: settings.get(Z_STEP_ANGLE),
-        units_per_rev: settings.get(Z_MICRONS_PER_REV),
-        microstepping: settings.get(MICRO_STEPS_MODE),
-        jerk: settings.get(Z_HOMING_JERK),
-        speed: settings.get(Z_HOMING_SPEED)
-      )
+      movement = motor_controller.next_movement
+      expect(movement.axis).to                          eq(:r)
+      expect(movement.action).to                        eq(MC_HOME)
+      expect(movement.parameter).to                     eq(UNITS_PER_REVOLUTION)
+      expect(movement.settings.step_angle).to           eq(settings.get(R_STEP_ANGLE))
+      expect(movement.settings.units_per_revolution).to eq(settings.get(R_MILLIDEGREES_PER_REV) / R_SCALE_FACTOR)
+      expect(movement.settings.microstepping_factor).to eq(settings.get(MICRO_STEPS_MODE))
+      expect(movement.settings.max_jerk).to             eq(settings.get(R_HOMING_JERK))
+      expect(movement.settings.speed).to                eq(settings.get(R_HOMING_SPEED) * R_SPEED_FACTOR)
+
+      movement = motor_controller.next_movement
+      expect(movement.axis).to                          eq(:r)
+      expect(movement.action).to                        eq(MC_MOVE)
+      expect(movement.parameter).to                     eq(settings.get(R_HOMING_ANGLE) / R_SCALE_FACTOR)
+      expect(movement.settings.step_angle).to           eq(settings.get(R_STEP_ANGLE))
+      expect(movement.settings.units_per_revolution).to eq(settings.get(R_MILLIDEGREES_PER_REV) / R_SCALE_FACTOR)
+      expect(movement.settings.microstepping_factor).to eq(settings.get(MICRO_STEPS_MODE))
+      expect(movement.settings.max_jerk).to             eq(settings.get(R_HOMING_JERK))
+      expect(movement.settings.speed).to                eq(settings.get(R_HOMING_SPEED) * R_SPEED_FACTOR)
+
+      movement = motor_controller.next_movement
+      expect(movement.axis).to                          eq(:z)
+      expect(movement.action).to                        eq(MC_HOME)
+      expect(movement.parameter).to                     eq(-2 * settings.get(Z_START_PRINT_POSITION))
+      expect(movement.settings.step_angle).to           eq(settings.get(Z_STEP_ANGLE))
+      expect(movement.settings.units_per_revolution).to eq(settings.get(Z_MICRONS_PER_REV))
+      expect(movement.settings.microstepping_factor).to eq(settings.get(MICRO_STEPS_MODE))
+      expect(movement.settings.max_jerk).to             eq(settings.get(Z_HOMING_JERK))
+      expect(movement.settings.speed).to                eq(settings.get(Z_HOMING_SPEED) * Z_SPEED_FACTOR)
 
       motor_controller.respond_to_interrupt_request
 
+      poll
+
       expect(motor_controller).to be_disabled
+
+      #expect(projector).to show_black
     end
   end
 
