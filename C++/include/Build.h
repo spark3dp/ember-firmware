82a64bd44e408c36669106394f2a8da42cf3ac93
Automated build machine <jason.lefley@autodesk.com>
2015-11-11 00:00:12 -0800

    Automated commit from build machine: update version number to 2.3.20151111.0

--- C++/include/Build.h
+++ C++/include/Build.h
@@ -26,7 +26,7 @@
 #define	BUILD_H
 
 // Date of build
-constexpr const char* BUILD_DATE = "20151110";
+constexpr const char* BUILD_DATE = "20151111";
 
 // Build attempt on BUILD_DATE
 constexpr const char* BUILD_NUMBER = "0";
--- ruby/lib/smith/definitions.rb
+++ ruby/lib/smith/definitions.rb
@@ -118,6 +118,6 @@ module Smith
   SETTINGS_ROOT_KEY = 'Settings'
   PRINT_FILE_SETTING = 'PrintFile'
   JOB_ID_SETTING = 'JobID'
-  BUILD_DATE = '20151110'
+  BUILD_DATE = '20151111'
   BUILD_NUMBER = '0'
 end
