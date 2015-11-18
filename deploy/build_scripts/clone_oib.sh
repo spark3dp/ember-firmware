8c4006710c76f8370c1df3e2d203dff6b9af8c0d
Jason Lefley <jason.lefley@autodesk.com>
2015-11-17 16:14:18 -0800

    use v2015.11 oib release

--- deploy/build_scripts/clone_oib.sh
+++ deploy/build_scripts/clone_oib.sh
@@ -3,12 +3,11 @@
 
 oib_git_url='https://github.com/RobertCNelson/omap-image-builder.git'
 
-# This is the commit we know works with our build scripts
-# omap-image-builder release v2015.10
-oib_commit_sha1='1ed9af7ce686250c5fa20fed18c097d3569df65c'
+# This is the release we know works with our build scripts
+oib_release='v2015.11'
 
 if [ ! -d omap-image-builder ]; then
   git clone ${oib_git_url}
 fi
 
-cd omap-image-builder && git checkout master && git pull origin master && git checkout ${oib_commit_sha1}
+cd omap-image-builder && git checkout master && git fetch && git checkout tags/${oib_release}
