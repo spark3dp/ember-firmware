ab7c93f6a6c55f30bab64cc31ccd79f0c32396c0
Richard Greene <richard.greene@autodesk.com>
2015-11-23 18:01:59 -0800

    adding spec for cancel before start (just first case so far) and handling of pause/resume/clear in motor controller, and factoring out common motor command expectations

--- tests/spec/startup_spec.rb
+++ tests/spec/startup_spec.rb
@@ -46,35 +46,7 @@ module Tests
 
       expect(motor_controller).to be_enabled
 
-      movement = motor_controller.next_movement
-      expect(movement.axis).to                          eq(:r)
-      expect(movement.action).to                        eq(MC_HOME)
-      expect(movement.parameter).to                     eq(UNITS_PER_REVOLUTION)
-      expect(movement.settings.step_angle).to           eq(settings.get(R_STEP_ANGLE))
-      expect(movement.settings.units_per_revolution).to eq(settings.get(R_MILLIDEGREES_PER_REV) / R_SCALE_FACTOR)
-      expect(movement.settings.microstepping_factor).to eq(settings.get(MICRO_STEPS_MODE))
-      expect(movement.settings.max_jerk).to             eq(settings.get(R_HOMING_JERK))
-      expect(movement.settings.speed).to                eq(settings.get(R_HOMING_SPEED) * R_SPEED_FACTOR)
-
-      movement = motor_controller.next_movement
-      expect(movement.axis).to                          eq(:r)
-      expect(movement.action).to                        eq(MC_MOVE)
-      expect(movement.parameter).to                     eq(settings.get(R_HOMING_ANGLE) / R_SCALE_FACTOR)
-      expect(movement.settings.step_angle).to           eq(settings.get(R_STEP_ANGLE))
-      expect(movement.settings.units_per_revolution).to eq(settings.get(R_MILLIDEGREES_PER_REV) / R_SCALE_FACTOR)
-      expect(movement.settings.microstepping_factor).to eq(settings.get(MICRO_STEPS_MODE))
-      expect(movement.settings.max_jerk).to             eq(settings.get(R_HOMING_JERK))
-      expect(movement.settings.speed).to                eq(settings.get(R_HOMING_SPEED) * R_SPEED_FACTOR)
-
-      movement = motor_controller.next_movement
-      expect(movement.axis).to                          eq(:z)
-      expect(movement.action).to                        eq(MC_HOME)
-      expect(movement.parameter).to                     eq(-2 * settings.get(Z_START_PRINT_POSITION))
-      expect(movement.settings.step_angle).to           eq(settings.get(Z_STEP_ANGLE))
-      expect(movement.settings.units_per_revolution).to eq(settings.get(Z_MICRONS_PER_REV))
-      expect(movement.settings.microstepping_factor).to eq(settings.get(MICRO_STEPS_MODE))
-      expect(movement.settings.max_jerk).to             eq(settings.get(Z_HOMING_JERK))
-      expect(movement.settings.speed).to                eq(settings.get(Z_HOMING_SPEED) * Z_SPEED_FACTOR)
+      expect_motor_controller_to_go_home
 
       motor_controller.respond_to_interrupt_request
 
