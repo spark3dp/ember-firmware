80290d0492d4b6fc6f707fbea13dcf9c3c455579
Jason Lefley <jason.lefley@autodesk.com>
2015-12-04 16:09:56 -0800

    netbeans updated these files

--- Utilities/i2c_boot/nbproject/Makefile-variables.mk
+++ Utilities/i2c_boot/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=i2c_boot
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/i2c_boot
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/i2c_boot
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=i2cboot.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/i2cboot.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/i2cboot.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
 CND_ARTIFACT_NAME_Release=i2c_boot
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/i2c_boot
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/i2c_boot
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
 CND_PACKAGE_NAME_Release=i2cboot.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/i2cboot.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/i2cboot.tar
 #
 # include compiler specific variables
 #
