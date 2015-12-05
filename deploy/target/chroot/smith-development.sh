15a73b832071310959fbde371f05e7f38ecc4b28
Jason Lefley <jason.lefley@autodesk.com>
2015-12-04 17:18:50 -0800

    add code to build device tree overlays when building development image

--- deploy/target/chroot/smith-development.sh
+++ deploy/target/chroot/smith-development.sh
@@ -16,6 +16,20 @@ fi
 
 echo "Log: (chroot) executing smith-development chroot script"
 
+# Build device tree overlays
+# We need BB-I2C2-0A00.dtbo included in the initramfs, otherwise it won't load
+dtc_overlays_commit='839093da04aee7f96f78b839c39d283d99bafc01'
+cd /opt
+git clone https://github.com/beagleboard/bb.org-overlays
+cd bb.org-overlays
+git checkout "${dtc_overlays_commit}"
+./dtc-overlay.sh
+make
+make install
+# Need to specify version of kernel that will end up in the image we generate
+update-initramfs -u -k $(echo "${kernel_pkg}" | cut -c13-)
+make clean
+
 # Disable rsync service
 #systemctl disable rsync.service
 
