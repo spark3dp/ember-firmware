7ec3add46bbd9744f87a2a619a4329c7b7c5a454
Automated build machine <richard.greene@autodesk.com>
2016-08-25 11:09:26 -0700

    Automated commit from build machine: update version number to 3.2.20160825.0

--- ruby/lib/smith/definitions.rb
+++ ruby/lib/smith/definitions.rb
@@ -124,6 +124,6 @@ module Smith
   SETTINGS_ROOT_KEY = 'Settings'
   PRINT_FILE_SETTING = 'PrintFile'
   JOB_ID_SETTING = 'JobID'
-  BUILD_DATE = '20160818'
-  BUILD_NUMBER = '1'
+  BUILD_DATE = '20160825'
+  BUILD_NUMBER = '0'
 end
